f24b2a09cf08567ce3b5a67153b95bb1cd8b0256
Marcelo Cesar <marcelop3@gmail.com>
2019-02-09 20:18:03 -0200

    revision for send

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Classic Arcade Game
+# Projeto - Frogger Classic Arcade Game
+
+## Índice
+
+* [Descrição](#descrição)
+* [Como Jogar](#contribuicao)
+* [Tecnologias e Dependências](#dependencias)
+* [Compatibilidade](#compatibilidade)
+
+## Descrição
+
+O jogo Frogger é um clássico dos anos 80. O objetivo do jogador é chegar até a águas sem ser atingido pelos inimigos (Bug's).
+
+
+## Como Jogar
+
+Clone este repositório e abra o arquivo ```index.html```. Utilize as as setas do teclado para movimentar o jogador.
+
+## Tecnologias e Dependências
+
+Este jogo foi desenvolvindo utilizando as tecnologias Javascript ES6+ (Vanilla), HTML5 canvas e CSS.
+
+## Compatibilidade
+
+O jogo foi testado nos seguintes navegadores (Firefox, Chrome, Edge).
--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 class Enemy {
+    /**
+    * @description Represents an Enemy
+    * @constructor
+    * @param {object} enemy - The object receiver default params { sprite = 'images/enemy-bug.png', x = 0, y = 0, speed = 1}
+    */
 	constructor({ sprite = 'images/enemy-bug.png', x = 0, y = 0, speed = 1 } = {}) {
 		this.sprite = sprite;
 		this.x = x;
@@ -6,6 +11,10 @@ class Enemy {
 		this.speed = speed;
 	}
 
+    /**
+    * @description update enemy position
+    * @param {number} dt - Animation frames
+    */
 	update(dt) {
 		this.x += 101 * this.speed * dt;
 
@@ -14,42 +23,66 @@ class Enemy {
 		}
 	}
 
+    /**
+    * @description Verify the collision between enemy and player
+    * @param {object} player - Player game
+    */
 	checkCollisions(player) {
 		if (this.x > player.x && this.x < player.x + 101 && this.y > player.y && this.y < player.y + 83) {
 			player.positionInitial();
 		}
 	}
 
+    /**
+    * @description Create object enemy in game
+    */
 	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
 }
 
 class Player {
+     /**
+    * @description Represents a Player
+    * @constructor
+    * @param {object} enemy - The object receiver default params { sprite = 'images/char-boy.png', x = 200, y = 380 }
+    */
 	constructor({ sprite = 'images/char-boy.png', x = 200, y = 380 } = {}) {
 		this.sprite = sprite;
 		this.x = x;
 		this.y = y;
 	}
 
+    /**
+    * @description reset position default player
+    * @param {number} dt - Animation frames
+    */
 	positionInitial() {
 		this.x = 200;
 		this.y = 380;
 	}
 
+    /**
+    * @description update player position
+    */
 	update() {
 		if (this.y < 0) {
 			this.positionInitial();
 		}
 	}
 
+    /**
+    * @description Create object player in game
+    */
 	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
 
+    /**
+    * @description move playe in game
+    * @param {string} key - move playe "up", "down", "left" and "right"
+    */
 	handleInput(key) {
-		let posX = 101;
-		let posY = 83;
 		switch (key) {
 			case 'up': {
 				if (this.y >= 0) {
@@ -78,10 +111,7 @@ class Player {
 		}
 	}
 }
-// Esta classe exige um método update(),
-// um render() e um handleInput().
 
-// Represente seus objetos como instâncias.
 const player = new Player();
 const enemy1 = new Enemy({ y: 60, x: -202, speed: 1.2 });
 const enemy2 = new Enemy({ y: 140, x: -808 });
@@ -89,13 +119,8 @@ const enemy3 = new Enemy({ y: 220, x: -404 });
 const enemy4 = new Enemy({ y: 60, x: -1600, speed: 1.4 });
 const enemy5 = new Enemy({ y: 140, x: -101 });
 const enemy6 = new Enemy({ y: 220, x: -606, speed: 1.1 });
-
 const allEnemies = [ enemy1, enemy2, enemy3, enemy4, enemy5, enemy6 ];
-// Coloque todos os objetos inimgos numa array allEnemies
-// Coloque o objeto do jogador numa variável chamada jogador.
 
-// Isto reconhece cliques em teclas e envia as chaves para seu
-// jogador. método handleInput(). Não é preciso mudar nada.
 document.addEventListener('keyup', function(e) {
 	var allowedKeys = {
 		37: 'left',
